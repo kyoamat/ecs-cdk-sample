ce66345fe9236e5d31bb2e4935c8e296efe08d45
kyoamat <ktatsuki198@gmail.com>
2024-06-18 13:55:06 +0900

    add sample

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a new IAM role
+    const executionRole = new iam.Role(this, 'ExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Attach the AmazonECSTaskExecutionRolePolicy to the role
+    executionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'));
+
+    new ecsp.ApplicationLoadBalancedFargateService(this, 'MyWebServer', {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('533266983710.dkr.ecr.ap-northeast-1.amazonaws.com/hands-on-repository:latest'),
+        executionRole: executionRole,
+      },
+      publicLoadBalancer: true,
+      desiredCount: 1,
+    });
   }
 }
